2a820ba616aed25106a78bbefd41a79f794f56c4
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-26 23:01:15 -0800

    3. Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	/* Delete left subtree */
+	binary_tree_delete(tree->left);
+	/* Delete right subtree */
+	binary_tree_delete(tree->right);
+	/* Free memory for current node */
+	free(tree);
+}
