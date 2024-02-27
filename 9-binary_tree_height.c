c7353bc34cad6c1e447810d0e710fb22de9f3b35
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-27 12:08:45 -0800

    9. Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t h_left = 0, hright = 0;
 
-	if (tree == NULL)
+	if (tree)
 	{
 		h_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		hright = tree->right ? 1 + binary_tree_height(tree->right) : 0;
