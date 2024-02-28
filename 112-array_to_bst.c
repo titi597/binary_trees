eda0092543137b965cc1fb751b68888e6360b893
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-28 02:34:17 -0800

    26. BST - Array to BST

---
+++ 112-array_to_bst.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - Builds a Binary Search Tree from an array
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
+ * Return: Pointer to the root node of the created BST, or NULL on failure
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root = NULL;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		if (bst_insert(&root, array[i]) == NULL)
+		{
+			/* If bst_insert returns NULL, there was a failure */
+			/* Free memory allocated for the tree and return NULL */
+			binary_tree_delete(root);
+			return (NULL);
+		}
+	}
+	return (root);
+}
