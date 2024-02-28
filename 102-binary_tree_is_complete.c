f633e3dfc03d7eb5834cf838a2e2dbb23764df71
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-28 01:33:04 -0800

    21. Is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: Size of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+		return (1);
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 int binary_tree_is_ancestor(const binary_tree_t *ancestor,
 		const binary_tree_t *node);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H */
